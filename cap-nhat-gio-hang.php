e540abadecfd3b9285825034bf5fd800fb872a7f
Thuc924 <ngohongthai05051994@gmail.com>
2020-12-16 07:33:28 +0700

    Chuc nang Login

--- cap-nhat-gio-hang.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	require_once __DIR__. "/autoload/autoload.php"; 
-	$key = intval(getInput("key"));
-	$qty = intval(getInput("qty"));
-
-	$_SESSION['cart'][$key]['qty'] = $qty;
-
-	echo 1;
- ?>
